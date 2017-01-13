1a9ab4981703f6799cf9526867d02ce43158a7fc
Laszlo Kantor <laszlo.kantor@semilab.hu>
2017-01-13 09:39:41 +0100

    Hello 3 line

--- Sample1/Sample1/Program.cs
+++ Sample1/Sample1/Program.cs
@@ -11,6 +11,7 @@ namespace Sample1
         {
             Console.WriteLine("Hello 1!");
             Console.WriteLine("Hello 2!");
+            Console.WriteLine("Hello 3!");
         }
     }
 }
